07deba38653aa7d7547682363b290b19726d110a
Mikhail Andreevich <mikhail_stroganov@bk.ru>
2023-09-06 21:28:18 +0300

    no final version

--- Povtorenie/Povtorenie/main.cpp
+++ Povtorenie/Povtorenie/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdlib.h> 
+#include <time.h>
 class FIO {
 	std::string name, surname, lastname;
 public:
@@ -44,19 +46,20 @@ class Student {
 	FIO student;
 	int id;
 	float srednyball;
-	bool stipuha;
-	bool funding;
+	std::string stipuha;
+	std::string funding;
 public:
 	Student() {
 		id = 0;
-		srednyball = 1.1;
-		stipuha = 0;
-		funding = 0;
-	}Student(FIO _student, int _id, float _sredball, bool _stipuha, bool _funding)
+		srednyball = 0.0;
+		stipuha = "no";
+		funding = "no";
+	}
+	Student(FIO _student, int _id, float _sredball, std::string _stipuha, std::string _funding)
 	{}
 	Student(const Student& a) : student(a.student), id(a.id), srednyball(a.srednyball), stipuha(a.stipuha), funding(a.funding)
 	{}
-	Student& operator =(Student& a) {
+	Student& operator =(const Student& a) {
 		if (this != &a) {
 			Student tmp(a);
 			this->swap(tmp);
@@ -70,6 +73,10 @@ public:
 		std::swap(this->stipuha, a.stipuha);
 		std::swap(this->funding, a.funding);
 	}
+	FIO get_student_FIO() {
+		return student;
+	}
+	
 	friend std::ostream& operator << (std::ostream& out, const Student& a);
 	friend std::istream& operator >>(std::istream& in, Student& a);
 	friend bool operator ==(const Student& a, const Student& b) {
@@ -78,20 +85,154 @@ public:
 	FIO get_FIO() {
 		return student;
 	}
+	int get_id() {
+		return id;
+	}
+	int GetRandomNumber()
+	{
+		srand(time(NULL));
+		int num = 10000000 + rand() % (99999999 - 10000000 + 1);
+
+		return num;
+	}
+
 };
 std::ostream& operator << (std::ostream& out, const Student& a) {
-	out << "\nFIO Student:\n" << a.student << "\nStudents bilet: " << a.id << "\nStipendia: " << a.stipuha << "\nFunding" << a.funding;
+	out << "\nFIO Student:\n" << a.student << "\nStudents bilet: " << a.id << "\nStipendia: " << a.stipuha << "\nFunding: " << a.funding << "\nSredny ball: "<< a.srednyball;
 	return out;
 }
 std::istream& operator>>(std::istream& input, Student& a) {
 	std::cout << "Input FIO Student:\n";
 	input >> a.student;
-	std::cout << "Input number of students bilet: ";
-	input >> a.id;
+	//std::cout << "Input number of students bilet: ";
+	//input >> a.id;
+	a.id = a.GetRandomNumber();
 	std::cout << "Input stipendia: ";
 	input >> a.stipuha;
+	
 	std::cout << "Input funding: ";
 	input >> a.funding;
-
+	std::cout << "Input sredball: ";
+	input >> a.srednyball;
 	return input;
+}
+class archive {
+	int capacity;
+	int size;
+	Student* list;
+public:
+	archive() : size(0), capacity(5) {
+		list = new Student[capacity];
+	}
+	archive(int _capacity) :
+		size(0), capacity(_capacity)
+	{
+		list = new Student[capacity];
+	}
+	archive(const archive& a) :
+		size(a.size), capacity(a.capacity), list(a.list)
+	{}
+	~archive() {
+		delete[] list;
+		list = nullptr;
+	}
+
+	void add(const Student& a) {
+		if (size >= capacity) {
+			int new_capacity = capacity * 2;
+			Student* new_list = new Student[new_capacity];
+			for (int i = 0; i < capacity; i++) {
+				new_list[i] = list[i];
+			}
+			delete[] list;
+			list = new_list;
+			capacity = new_capacity;
+		}
+		list[size] = a;
+		size++;
+	}
+	void search_student(const FIO& a) {
+		int counter = 0;
+		for (int i = 0; i < size; i++) {
+			if (a == list[i].get_student_FIO()) {
+				counter++;
+			}
+		}
+		int* doc_list = new int[counter];
+		int counter1 = 0;
+		if (counter != 0) {
+			for (int i = 0; i < size; i++) {
+				if (a == list[i].get_student_FIO()) {
+					doc_list[counter1] = i;
+					counter1++;
+				}
+			}
+			for (int i = 0; i < counter1; i++) {
+				std::cout << "\n" << doc_list[i] + 1 << ") " << list[i];
+			}
+
+		}
+		else {
+			std::cout << "not found";
+		}
+	}
+	void remove(int _count) {
+		int index = _count - 1;
+		for (int i = index; i < size - 1; i++) {
+			if (list[i].get_id() == _count) {
+				list[i] = list[i + 1];
+			}
+			
+		}
+		size--;
+	}
+	
+	void display() {
+		for (int i = 0; i < size; i++) {
+			std::cout << "\n" << i + 1 << ") " << list[i];
+		}
+	}
+};
+int main() {
+	int choose = 0;
+	Student new_student;
+	archive baza;
+	FIO student;
+	int index = 0, sid=0;
+	while (true) {
+		std::cout << "1.Add a new student.\n2.Show the list.\n3.Search student.\n4.Remove.\n5.Exit\nYour choise:";
+		std::cin >> choose;
+		switch (choose) {
+		case 1:
+
+			std::cin >> new_student;
+			baza.add(new_student);
+			choose = 0;
+			break;
+		case 2:
+			baza.display();
+			std::cout << "\n";
+			system("pause");
+			choose = 0;
+			break;
+		case 3:
+			
+			std::cin >> student;
+			baza.search_student(student);
+			choose = 0;
+			system("pause");
+			break;
+		case 4:
+			std::cout << "Input num of sudbillet: ";
+			std::cin >> sid;
+			baza.remove(sid);
+			choose = 0;
+			break;
+		case 5:
+			return 0;
+		}
+		system("cls");
+	}
+	std::cout << "Bye-bye";
+	return 0;
 }
